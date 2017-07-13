09f833dc742170d57c7166a0c70ae9fb7271d548
Sebastien Blanchet <s3blanch@edu.uwaterloo.ca>
2017-07-13 11:50:18 -0400

    Update Lab 3a, Simulink 2012

---
+++ Lab3/MATLAB/trackplot3.m
@@ -0,0 +1,61 @@
+function [fig] = trackplot3(sim, exp, ms, fignum)
+    
+    tlt = sprintf('Tracking Plots for T_s = %0.f ms of %s vs. %s', ...
+        ms, sim.txt, exp.txt);
+
+    fig = figure(fignum);
+    subplot(4,1,1)
+    hold on
+
+    %ref
+    plot(sim.yref(:,1),sim.yref(:,2), '--','linewidth',1);
+    plot(exp.t,exp.yref, 'linewidth', 1) ;
+    hold off
+    title(tlt);
+    xlabel('');
+    ylabel('y{ref} [m]');
+    set(gca,'xticklabel',{[]});
+    legend(sim.txt, exp.txt); 
+
+    %Control Signal
+    subplot(4,1,2)
+    hold on
+    plot(sim.u(:,1),sim.u(:,2),'--','linewidth',1);
+    plot(exp.t,exp.u,'linewidth',1);
+    hold off
+    title('');
+    xlabel('');
+    ylabel('u [V]');
+    legend(sim.txt, exp.txt); 
+
+    % Gear Angle
+    subplot(4,1,3)
+    hold on
+    plot(sim.ServoAng(:,1),sim.ServoAng(:,2),'--','linewidth',1);
+    plot(exp.t, exp.ServoAng, 'linewidth',1);
+    hold off
+    title('');
+    set(gca,'xticklabel',{[]});
+    ylabel('ServoAng [rad]');
+    legend(sim.txt, exp.txt); 
+    
+    
+    % Ball Psn
+    subplot(4,1,4)
+    hold on
+    plot(sim.BallPosn(:,1),sim.BallPosn(:,2),'--','linewidth',1);
+    plot(exp.t, exp.BallPosn,'linewidth',1);
+    hold off
+    title('');
+    set(gca,'xticklabel',{[]});
+    xlabel('Time [sec]');
+    ylabel('BallPosn [m]');
+    legend(sim.txt, exp.txt); 
+    
+    ms = num2str(ms);
+    plotstr=strcat('Plots\Tracking_',sim.txt,'_',exp.txt, '_', ms, 'ms');   
+    plotstr=char(plotstr);
+    print(fignum,'-djpeg',plotstr);
+
+end
+
